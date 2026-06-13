7410c109ab65feda30b2e2fff7516b323648f4b9
daferur-lang <daferur@gmail.com>
2026-06-13 10:43:02 +0200

    feat: deploy Facturas Pro PWA to GitHub Pages

---
+++ sw.js
@@ -0,0 +1,6 @@
+const CACHE = 'facturas-pro-v1';
+const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json', './icons/icon.svg',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
